f9978a157a86eda813f7325d3134bc91926b36bb
salva <atlas200x@gmail.com>
2023-07-09 15:34:49 -0500

    feat!: replace ANSI colors with colored lib

--- print_matrices.py
+++ print_matrices.py
@@ -1,31 +1,35 @@
 import main
 
+from colored import Back
+
 
 def imprimir_numeros_coloreados(numeros):
     for numero in numeros:
         if numero == 0:
             # Imprimir número 0 en color negro
-            print(" " + "\033[30m{}\033[0m".format(numero), end=" ")
+            print(Back.RGB(0, 0, 0) + " " + str(numero), end=" ")
         elif numero == 16:
             # Imprimir número 16 en color blanco
-            print("\033[97m{}\033[0m".format(numero), end=" ")
+
+            print(Back.RGB(255, 255, 255) + " " + str(numero), end=" ")
         else:
             # Calcular el valor de escala entre blanco y negro
-            escala = int(232 + (23 * (numero - 1) / 15))
+            escala = int((16 * numero) - 1)
 
             # Imprimir números del 1 al 15 con colores intermedios
             # Correcciones en estetica al imprimir la matriz
             if len(str(numero)) == 1:
-                print(" " + "\033[38;5;{}m{}\033[0m".format(escala, numero), end=" ")
+                print(Back.RGB(escala, escala, escala) + " " + str(numero), end=" ")
             else:
-                print("\033[38;5;{}m{}\033[0m".format(escala, numero), end=" ")
+                print(Back.RGB(escala, escala, escala) + str(numero), end=" ")
 
 
 # imprimir la matriz de forma entendible
 def imprimir_matriz_8x8(matriz):
     for fila in matriz:
         imprimir_numeros_coloreados(fila)
-        print("")
+        # Rompe el color blanco aquí
+        print(Back.RGB(0, 0, 0) + " ")
 
 
 # Impresion de las matrices de todos los numeros
@@ -34,5 +38,3 @@ for i in range(0, 10):
     print("")
     imprimir_matriz_8x8(main.average_number[i])
     print("")
-
-
